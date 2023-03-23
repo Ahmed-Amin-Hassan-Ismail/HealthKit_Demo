8fb084dde5308f3d8135b5a56ad40d96bf6bd410
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2023-03-23 15:43:30 +0200

    initialize and create xcode IDE

---
+++ HealthKit_Demo/HealthKit_Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthKit_Demo
+//
+//  Created by Ahmed Amin on 23/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
